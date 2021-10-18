558226a4c801a3b867156a42549b8e01b82d98a9
afolabiopakunle <opakunleafolabi@gmail.com>
2021-10-18 23:08:48 +0100

    subscribe works

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { interval } from 'rxjs';
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
   styleUrls: [ './app.component.css' ]
 })
-export class AppComponent  {
-  name = 'Angular '
+export class AppComponent implements OnInit  {
+  
+  
+  ngOnInit() {
+    interval(1000).subscribe(count => {
+      console.log(count)
+    })
+  }
 }

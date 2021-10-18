62fd1ba6e82b53e8666a7dab1ff3341e74d04696
afolabiopakunle <opakunleafolabi@gmail.com>
2021-10-18 23:52:17 +0100

    test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,20 @@
 import { Component, OnInit } from '@angular/core';
-import { interval } from 'rxjs';
+import { interval, Subscription } from 'rxjs';
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
   styleUrls: [ './app.component.css' ]
 })
 export class AppComponent implements OnInit  {
-  
+ 
+  mySubscription: Subscription;
   
   ngOnInit() {
-    interval(1000).subscribe(count => {
-      // console.log(count)
+  this.mySubscription =  interval(1000).subscribe(count => {
+      console.log(count)
+      if(count === 10) {
+        this.mySubscription.unsubscribe
+      }
     })
   }
 }
